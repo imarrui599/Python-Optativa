8f3753047ce296246c3f690fcae8262a9895fab7
imarrui599 <imarrui599@ieszaidinvergeles.org>
2025-10-07 13:41:38 +0200

    ejer1.py

---
+++ Ej2/ejer1.py
@@ -0,0 +1,8 @@
+'''Escribir un programa que pregunte el nombre del usuario en la consola y un número
+entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces
+como el número introducido.'''
+
+nombre = input("Introducza su nombre:")
+num = input("Introduzca su numero entero")
+
+print((nombre + "\n") * int(3))
