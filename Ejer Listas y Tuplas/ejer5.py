8000a785b31f49362bf87ab762680acd8a4455b4
imarrui599 <imarrui599@ieszaidinvergeles.org>
2025-12-03 09:46:25 +0100

    ejer5.py

---
+++ Ejer Listas y Tuplas/ejer5.py
@@ -0,0 +1,7 @@
+'''Escribir un programa que almacene en una lista los números del 1 al 10 y los
+muestre por pantalla en orden inverso separados por comas'''
+
+numeros = list(range(1, 11))
+numeros.reverse()
+
+print(numeros)
