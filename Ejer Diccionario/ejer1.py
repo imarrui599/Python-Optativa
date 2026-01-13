5356fd339a70a339582fbf93cde19ecab70129f0
imarrui599 <imarrui599@ieszaidinvergeles.org>
2026-01-13 13:35:23 +0100

    actividad 1 diccionario completada

---
+++ Ejer Diccionario/ejer1.py
@@ -0,0 +1,13 @@
+'''Escribir un programa que guarde en una variable el diccionario {'Euro':'€',
+'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y muestre su
+símbolo o un mensaje de aviso si la divisa no está en el diccionario.'''
+
+monedas = {'Euro':'€', 'Dollar': '$', 'Yen':'¥'}
+divisa = input("Introduce una divisa: ")
+
+if divisa.title() in monedas:
+    print(monedas[divisa.title()])
+else:
+    print("La divisa no está en el diccionario")
+
+
